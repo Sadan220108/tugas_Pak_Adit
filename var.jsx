a73ea61b46f357cfa77cec092b780c427407b88b
Sadan Robbany <sadanrobbany@gmail.com>
2026-08-21 22:08:56 +0700

    Soal Variabel

---
+++ var.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function letvar(){
+    let murid1 = budi
+    const murid2 = siti
+    const keluaran = "kowkxwkxowoxw";
+
+    console.log(murid1);
+    console.log(murid2);
+    console.log(keluaran);
+
+    return(
+        <View>
+        <Text>{murid2}</Text>
+        </View>
+    );
+}
