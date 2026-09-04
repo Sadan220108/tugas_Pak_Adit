a89685376432505effbd21e17504892f6403c777
Sadan Robbany <sadanrobbany@gmail.com>
2026-09-04 08:37:56 +0700

    index

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+import Varscreen from "./component/scom.jsx";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Varscreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+      alignItems: "center",
+      justifyContent: "top",
+  },
+});
